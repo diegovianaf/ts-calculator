9e9917b186ce37790f252d499b5e7a007ea6c56f
Diego Viana <diegovianaf7@gmail.com>
2022-12-14 13:32:13 -0300

    add logic using typescript

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,41 @@
 "use strict";
-console.log("It's working");
+const calculatorElement = document.getElementById('calculator');
+const resultElement = document.getElementById('result');
+function evaluate(expression) {
+    // not so safe, used only for learning
+    try {
+        if (expression.match(/[a-zA-Z&#$<>{}]/g))
+            throw new Error();
+        return new Function(`return (${expression})`)();
+    }
+    catch (error) {
+        return null;
+    }
+}
+function isNumber(value) {
+    if (typeof value === 'number') {
+        return !isNaN(value) && isFinite(value);
+    }
+    else
+        return false;
+}
+function round(value) {
+    return Math.round(value * 1000) / 1000;
+}
+function calculate() {
+    localStorage.setItem('calculadora', calculatorElement.value);
+    const lines = calculatorElement.value.split(/\r?\n/).map(evaluate);
+    resultElement.innerHTML = `<div>${lines
+        .map((line) => `<div>${isNumber(line) ? round(line) : '---'}</div>`)
+        .join('')}</div>`;
+    const total = round(lines.filter(isNumber).reduce((acc, cur) => acc + cur, 0));
+    resultElement.innerHTML += `<div id="total">${total}</div>`;
+    const totalElement = document.getElementById('total');
+    totalElement.addEventListener('click', () => {
+        navigator.clipboard.writeText(total.toString());
+    });
+}
+calculatorElement.value = localStorage.getItem('calculator') || '';
+calculatorElement.addEventListener('input', calculate);
+calculate();
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,49 @@
-console.log("It's working")
+const calculatorElement = document.getElementById('calculator') as HTMLInputElement
+const resultElement = document.getElementById('result') as HTMLElement
+
+function evaluate(expression: string): number | null {
+  // not so safe, used only for learning
+  try {
+    if (expression.match(/[a-zA-Z&#$<>{}]/g)) throw new Error()
+    return new Function(`return (${expression})`)()
+  } catch (error) {
+    return null
+  }
+}
+
+function isNumber(value: unknown): value is number {
+  if (typeof value === 'number') {
+    return !isNaN(value) && isFinite(value)
+  } else return false
+}
+
+function round(value: number) {
+  return Math.round(value * 1000) / 1000
+}
+
+function calculate() {
+  localStorage.setItem('calculadora', calculatorElement.value)
+
+  const lines = calculatorElement.value.split(/\r?\n/).map(evaluate)
+
+  resultElement.innerHTML = `<div>${lines
+    .map((line) => `<div>${isNumber(line) ? round(line) : '---'}</div>`)
+    .join('')}</div>`
+
+  const total = round(
+    lines.filter(isNumber).reduce((acc, cur) => acc + cur, 0)
+  )
+
+  resultElement.innerHTML += `<div id="total">${total}</div>`
+
+  const totalElement = document.getElementById('total') as HTMLElement
+  totalElement.addEventListener('click', () => {
+    navigator.clipboard.writeText(total.toString())
+  })
+}
+
+calculatorElement.value = localStorage.getItem('calculator') || ''
+
+calculatorElement.addEventListener('input', calculate)
+
+calculate()
